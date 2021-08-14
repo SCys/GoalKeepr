4bd30881bb0d0020b102739d3a8c2593953ec5a5
SCys <me@iscys.com>
2021-08-14 22:54:05 +0800

    + add default handler

--- handlers/default.py
+++ handlers/default.py
@@ -1,6 +1,11 @@
 from aiogram import Bot, types
+from manager import manager
+
+logger = manager.logger
 
 
+@manager.register("message")
 async def default(message: types.Message, bot: Bot):
     # await bot.forward_message(from_chat_id=message.chat.id, chat_id=message.chat.id, message_id=message.message_id)
+    # logger.debug(f"Message: {message}")
     pass
