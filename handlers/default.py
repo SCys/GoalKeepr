7c19526741e50ca8621dc62b9d3ea539512af923
SCys <me@iscys.com>
2021-08-14 22:56:13 +0800

    +1

--- handlers/default.py
+++ handlers/default.py
@@ -1,11 +1,12 @@
-from aiogram import Bot, types
+from aiogram import types
+from aiogram.dispatcher.storage import FSMContext
 from manager import manager
 
 logger = manager.logger
 
 
 @manager.register("message")
-async def default(message: types.Message):
+async def new_members(msg: types.Message, ctx: FSMContext):
     # await bot.forward_message(from_chat_id=message.chat.id, chat_id=message.chat.id, message_id=message.message_id)
-    # logger.debug(f"Message: {message}")
+    # logger.debug(f"Message: {msg}")
     pass
