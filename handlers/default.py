0fbfcc003206ab0f402341b16289c0c2dbda05d8
SCys <me@iscys.com>
2021-08-14 22:55:31 +0800

    +1

--- handlers/default.py
+++ handlers/default.py
@@ -5,7 +5,7 @@ logger = manager.logger
 
 
 @manager.register("message")
-async def default(message: types.Message, bot: Bot):
+async def default(message: types.Message):
     # await bot.forward_message(from_chat_id=message.chat.id, chat_id=message.chat.id, message_id=message.message_id)
     # logger.debug(f"Message: {message}")
     pass
