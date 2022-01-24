9d6da17e46f962d72d5f032c7c72f0eab8153174
SCys <me@iscys.com>
2022-01-24 20:50:48 +0800

    * minor changes

--- handlers/commands/id.py
+++ handlers/commands/id.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from aiogram import types
 from aiogram.dispatcher.storage import FSMContext
 from manager import manager
@@ -15,5 +17,8 @@ async def id(msg: types.Message, state: FSMContext):
     if not user or not user.id:
         return
 
-    await msg.reply(f"""id:\t{user.id}\nname:\t{user.full_name}\nbot:\t{user.is_bot}""", disable_notification=True)
+    msg_reply = await msg.reply(f"""id:\t{user.id}\nname:\t{user.full_name}\nbot:\t{user.is_bot}""", disable_notification=True)
     logger.info(f"chat {msg.chat.id}({msg.chat.title}) msg {msg.message_id} user {user.id}({user.first_name})")
+
+    chat = msg.chat
+    await manager.lazy_delete_message(chat.id, msg_reply.id, msg.date + timedelta(seconds=5))
