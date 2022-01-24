e9547ec68868b33f1bd9e8cf27cfdf1017873ae8
SCys <me@iscys.com>
2022-01-24 20:48:45 +0800

    + add asr and id command

--- handlers/commands/__init__.py
+++ handlers/commands/__init__.py
@@ -1,2 +1,4 @@
 from .k import k
 from .tts import tts
+from .id import id
+from .asr import asr
---
+++ handlers/commands/id.py
@@ -0,0 +1,19 @@
+from aiogram import types
+from aiogram.dispatcher.storage import FSMContext
+from manager import manager
+
+logger = manager.logger
+
+
+@manager.register("message", commands=["id"])
+async def id(msg: types.Message, state: FSMContext):
+    if msg.reply_to_message:
+        user = msg.reply_to_message.from_user
+    else:
+        user = msg.from_user
+
+    if not user or not user.id:
+        return
+
+    await msg.reply(f"""id:\t{user.id}\nname:\t{user.full_name}\nbot:\t{user.is_bot}""", disable_notification=True)
+    logger.info(f"chat {msg.chat.id}({msg.chat.title}) msg {msg.message_id} user {user.id}({user.first_name})")
