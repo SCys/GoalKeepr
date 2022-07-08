c732ad953ca59df033024444aa230c466fede6d5
SCys <me@iscys.com>
2022-07-09 01:57:44 +0800

    * kick 命令不限制所在群主功能，只有权限即可

--- handlers/commands/k.py
+++ handlers/commands/k.py
@@ -15,21 +15,20 @@ logger = manager.logger
 @manager.register("message", commands=["k"])
 async def k(msg: types.Message, state: FSMContext):
     chat = msg.chat
-    if chat.type not in SUPPORT_GROUP_TYPES:
-        return
-
     user = msg.from_user
+    prefix = f"chat {chat.id}({chat.title}) msg {msg.message_id}"
 
     # check permission
     if not user or not await manager.is_admin(chat, user):
-        logger.warning(f"chat {chat.id}({chat.title}) msg {msg.message_id} user {user.id}({user.first_name}) is not admin")
+        logger.warning(f"{prefix} user {user.id}({user.first_name}) is not admin")
         return
 
     msgReply = msg.reply_to_message
     if not msgReply:
+        logger.info(f"{prefix} no reply message")
         return
 
-    # checkout target member
+    # 如果回复的是一个新加入信息，则直接踢掉用户
     if msgReply.new_chat_members:
         for member in msgReply.new_chat_members:
             resp = await kick_member(chat, msg, user, member)
@@ -39,6 +38,7 @@ async def k(msg: types.Message, state: FSMContext):
 
     # ignore
     elif msgReply.left_chat_member:
+        logger.info(f"{prefix} is left chat member message, ignored")
         return
 
     resp = await kick_member(chat, msg, user, msgReply.from_user)
@@ -57,20 +57,18 @@ async def kick_member(chat: types.Chat, msg: types.Message, administrator, membe
 
     id = member.id
 
+    prefix = f"chat {chat.id}({chat.title}) msg {msg.message_id}"
+
     # baned 60s
     if not await chat.kick(id, until_date=timedelta(seconds=BAN_MEMBER)):
-        logger.warning(
-            f"chat {chat.id}({chat.title}) msg {msg.message_id} user {administrator.id}({administrator.first_name}) failed to kick {id}"
-        )
+        logger.warning(f"{prefix} user {id}({member.first_name}) is not kickable, maybe he is administrator")
         return
 
     # 踢掉的用户将会保持在Baned状态，一定时间
     # await chat.unban(id)
 
-    logger.info(f"chat {chat.id}({chat.title}) msg {msg.message_id} member {id}({manager.user_title(member)}) is kicked")
-
+    logger.info(f"{prefix} user {id}({member.first_name}) is kicked")
     username = manager.user_title(member)
-
     return await msg.answer(
         f"{username} 被剔除/kicked",
         disable_web_page_preview=True,
