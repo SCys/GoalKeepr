922dd46e6a93a0871d05ed880ee88682c044c6f1
SCys <me@iscys.com>
2022-07-09 01:58:55 +0800

    minor changes

--- handlers/commands/k.py
+++ handlers/commands/k.py
@@ -23,26 +23,26 @@ async def k(msg: types.Message, state: FSMContext):
         logger.warning(f"{prefix} user {user.id}({user.first_name}) is not admin")
         return
 
-    msgReply = msg.reply_to_message
-    if not msgReply:
+    msg_reply = msg.reply_to_message
+    if not msg_reply:
         logger.info(f"{prefix} no reply message")
         return
 
     # 如果回复的是一个新加入信息，则直接踢掉用户
-    if msgReply.new_chat_members:
-        for member in msgReply.new_chat_members:
+    if msg_reply.new_chat_members:
+        for member in msg_reply.new_chat_members:
             resp = await kick_member(chat, msg, user, member)
             await manager.lazy_delete_message(chat.id, resp.message_id, msg.date + timedelta(seconds=DELETED_AFTER))
 
         return
 
     # ignore
-    elif msgReply.left_chat_member:
+    elif msg_reply.left_chat_member:
         logger.info(f"{prefix} is left chat member message, ignored")
         return
 
-    resp = await kick_member(chat, msg, user, msgReply.from_user)
-    for i in [msg, resp, msgReply]:
+    resp = await kick_member(chat, msg, user, msg_reply.from_user)
+    for i in [msg, resp, msg_reply]:
         await manager.lazy_delete_message(chat.id, i.message_id, msg.date + timedelta(seconds=DELETED_AFTER))
 
 
