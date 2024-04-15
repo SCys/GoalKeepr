8809481da4e00d6b1b403b8dd7eb01eb2995ccef
yanshen.chen <yanshen.chen@geartech.com>
2024-04-15 16:51:43 +0800

    Update model version to gemini-1.5-pro

--- handlers/commands/chat/func_txt.py
+++ handlers/commands/chat/func_txt.py
@@ -51,7 +51,7 @@ async def generate_text(chat: types.Chat, member: types.ChatMember, prompt: str)
     # request openai v1 like api
     url = f"{host}/v1/chat/completions"
     data = {
-        "model": "gemini-1.0-pro",
+        "model": "gemini-1.5-pro",
         "max_tokens": 30720,  # 32k for gemini-pro
         "temperature": 0.9,
         "top_p": 1,
