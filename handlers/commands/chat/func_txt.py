dfd9b1869c27ff4b7acb35f921921ea92e57e988
SCys <me@iscys.com>
2024-06-01 21:16:40 +0800

    Refactor model selection and input truncation in generate_text function

--- handlers/commands/chat/func_txt.py
+++ handlers/commands/chat/func_txt.py
@@ -1,56 +1,71 @@
+from os import name
+from types import ClassMethodDescriptorType
 from aiogram import types
+from dataclasses import dataclass
 from manager import manager
 from orjson import loads, dumps
 from aiohttp import ClientTimeout
 
 from .utils import count_tokens
+from dataclasses import dataclass
 
 logger = manager.logger
 
+
+@dataclass
+class ModelDescription:
+    name: str
+    input_length: int
+    output_length: int
+    rate_minute: float
+    rate_daily: float
+
+
 CONVERSATION_TTL = 3600
+DEFUALT_MODEL = "gemini-1.0-pro"
 SUPPORTED_MODELS = {
-    "gemini-1.0-pro": {
-        "name": "Gemini 1.0 Pro",
-        "input_length": 30720,
-        "output_length": 2048,
-        "rate_minute": 60,
-        "rate_daily": 0,
-    },
-    "gemini-1.5-pro": {
-        "name": "Gemini 1.5 Pro",
-        "input_length": 1048576,
-        "output_length": 8192,
-        "rate_minute": 0.5,
-        "rate_daily": 1000,
-    },
-    "mixtral-8x7b-32768": {
-        "name": "Mixtral 8x7b from Groq",
-        "input_length": 32768,
-        "output_length": 2048,
-        "rate_minute": 0,
-        "rate_daily": 0,
-    },
-    "llama3-8b-8192": {
-        "name": "Llama3 8b from Groq",
-        "input_length": 8192,
-        "output_length": 2048,
-        "rate_minute": 0,
-        "rate_daily": 0,
-    },
-    "llama3-70b-8192": {
-        "name": "Llama3 70b from Groq",
-        "input_length": 8192,
-        "output_length": 2048,
-        "rate_minute": 0,
-        "rate_daily": 0,
-    },
-    "gemma-7b-it": {
-        "name": "Gemma 7b IT from Groq",
-        "input_length": 4096,
-        "output_length": 2048,
-        "rate_minute": 0,
-        "rate_daily": 0,
-    },
+    "gemini-1.0-pro": ModelDescription(
+        name="Gemini 1.0 Pro",
+        input_length=30720,
+        output_length=2048,
+        rate_minute=60,
+        rate_daily=0,
+    ),
+    "gemini-1.5-pro": ModelDescription(
+        name="Gemini 1.5 Pro",
+        input_length=1048576,
+        output_length=8192,
+        rate_minute=0.5,
+        rate_daily=1000,
+    ),
+    "mixtral-8x7b-32768": ModelDescription(
+        name="Mixtral 8x7b from Groq",
+        input_length=32768,
+        output_length=2048,
+        rate_minute=0,
+        rate_daily=0,
+    ),
+    "llama3-8b-8192": ModelDescription(
+        name="Llama3 8b from Groq",
+        input_length=8192,
+        output_length=2048,
+        rate_minute=0,
+        rate_daily=0,
+    ),
+    "llama3-70b-8192": ModelDescription(
+        name="Llama3 70b from Groq",
+        input_length=8192,
+        output_length=2048,
+        rate_minute=0,
+        rate_daily=0,
+    ),
+    "gemma-7b-it": ModelDescription(
+        name="Gemma 7b IT from Groq",
+        input_length=4096,
+        output_length=2048,
+        rate_minute=0,
+        rate_daily=0,
+    ),
 }
 
 
@@ -71,8 +86,9 @@ async def generate_text(chat: types.Chat, member: types.User, prompt: str):
         {"role": "user", "content": prompt},
     ]
 
-    MODEL_NAME = "gemini-1.0-pro"
-    MODEL_INPUT_LENGTH = SUPPORTED_MODELS[MODEL_NAME]["input_length"]
+    # first key as default model
+    model_name = DEFUALT_MODEL
+    model_input_length = SUPPORTED_MODELS[model_name].input_length
 
     rdb = await manager.get_redis()
     if rdb:
@@ -97,12 +113,17 @@ async def generate_text(chat: types.Chat, member: types.User, prompt: str):
         model_person = settings_person.get("model")
 
         if model_person in SUPPORTED_MODELS:
-            MODEL_NAME = model_person
+            model_name = model_person
         elif model_global in SUPPORTED_MODELS:
-            MODEL_NAME = model_global
+            model_name = model_global
 
-        MODEL_INPUT_LENGTH = SUPPORTED_MODELS[MODEL_NAME]["input_length"]
-        truncate_input = min(MODEL_INPUT_LENGTH * 0.99, MODEL_INPUT_LENGTH - 1024)
+        # fallback to default model
+        if model_name not in SUPPORTED_MODELS:
+            model_name = DEFUALT_MODEL
+
+        model_input_length = SUPPORTED_MODELS[model_name].input_length
+
+        truncate_input = min(model_input_length * 0.99, model_input_length - 1024)
 
         # 从Redis获取之前的对话历史
         prev_chat_history = await rdb.get(f"chat:history:{member.id}")
@@ -122,8 +143,8 @@ async def generate_text(chat: types.Chat, member: types.User, prompt: str):
     # request openai v1 like api
     url = f"{host}/v1/chat/completions"
     data = {
-        "model": MODEL_NAME,
-        "max_tokens": MODEL_INPUT_LENGTH,
+        "model": model_name,
+        "max_tokens": model_input_length,
         "temperature": 0.75,
         # "top_p": 1,
         # "top_k": 1,
@@ -133,13 +154,10 @@ async def generate_text(chat: types.Chat, member: types.User, prompt: str):
         ],
     }
 
-    # if MODEL_NAME in ["mixtral-8x7b-32768", "llama2-70b-4096", "gemma-7b-it"]:
-    #     del data["top_k"]
-
     # show use model info
-    logger.info(f"chat {chat.id} user {member.id} generate txt use model {MODEL_NAME}({SUPPORTED_MODELS[MODEL_NAME]['name']})")
+    logger.info(f"chat {chat.id} user {member.id} generate txt use model {model_name}({model_name})")
 
-    session = await manager.bot.session.create_session()
+    session = await manager.bot.session.create_session()  # type: ignore
     async with session.post(
         url,
         json=data,
@@ -172,4 +190,4 @@ async def generate_text(chat: types.Chat, member: types.User, prompt: str):
             chat_history.append({"role": "assistant", "content": text})
             await rdb.set(f"chat:history:{member.id}", dumps(chat_history), ex=CONVERSATION_TTL)
 
-        return text + f"\n\nPower by {SUPPORTED_MODELS[MODEL_NAME]['name']}"
+        return text + f"\n\nPower by {model_name}"
