ae6a8aad0851fa7f9669f0891dc6c01af2b91fa8
SCys <me@iscys.com>
2024-08-24 17:30:24 +0800

    Refactor image.py to remove commented out code and optimize task creation

--- handlers/commands/image.py
+++ handlers/commands/image.py
@@ -170,18 +170,18 @@ async def process_task(task: Task):
     size = "512x512"
     step = 4
 
-    try:
-        if prompt.startswith("large "):
-            size = "768x1024"
-            step = 8
-            prompt = prompt[6:]
-            logger.info(f"{prefix} generate large image")
-        elif prompt.startswith("icon "):
-            size = "128x128"
-            prompt = prompt[5:]
-            logger.info(f"{prefix} generate icon")
-    except:
-        logger.exception(f"{prefix} parse prompt error")
+    # try:
+    #     if prompt.startswith("large "):
+    #         size = "768x1024"
+    #         step = 8
+    #         prompt = prompt[6:]
+    #         logger.info(f"{prefix} generate large image")
+    #     elif prompt.startswith("icon "):
+    #         size = "128x128"
+    #         prompt = prompt[5:]
+    #         logger.info(f"{prefix} generate icon")
+    # except:
+    #     logger.exception(f"{prefix} parse prompt error")
 
     try:
         resp = await sd_api.txt2img(endpoint, prompt, 1, size=size, step=step)
