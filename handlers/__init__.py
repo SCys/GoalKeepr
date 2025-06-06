fc839e7cf31a98921438f42b694c71e015057bb6
SCys <me@iscys.com>
2025-06-06 16:22:12 +0800

    新增聊天管理员设置回调函数，优化处理程序导入，提升代码结构清晰度

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,4 +1,4 @@
-# from .default import default
 from .member_captcha import member_captcha, new_member_callback
 from . import commands
+from .commands import chat_admin_settings_callback
 from .default import default_handler
